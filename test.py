c3ef705427535a9621590725fd6608ca9d9a3246
Nicholas Gabriel <macholas@macholas.hsd1.dc.comcast.net>
2022-11-09 11:12:03 -0500

    initial commit, test of layers

---
+++ test.py
@@ -0,0 +1,14 @@
+from hgcn.models.encoders import HNN,HGCN
+from config import parser
+import torch
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.dec_dims[0] = sum(args.enc_dims)
+    enc = HGCN(1.,args)
+    dec = HNN(1.,args)
+    n = 100
+    x,adj = torch.rand([10,60]),torch.randint(0,10,(2,10),dtype=torch.int64)
+    A = torch.sparse_coo_tensor(adj,torch.ones(adj.shape[1]),size=(10,10))
+    
+    z = enc.encode(x,A)
