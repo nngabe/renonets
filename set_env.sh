4e8d404fdbee9cbaef8caffc6b90d2b286ec40b6
Nicholas Gabriel <macholas@macholas.local>
2023-01-18 16:01:12 -0500

    fixed set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-export HPGN_HOME=$(pwd)
-export HGCN_HOME="$HPGN_HOME/hgcn"
-export LOG_DIR="$HGCN_HOME/logs"
-export PYTHONPATH="$HPGN_HOME:$PYTHONPATH"
-export PYTHONPATH="$HGCN_HOME:$PYTHONPATH"
-export DATAPATH="$HPGN_HOME/data"
+export COSYNN_HOME=$(pwd)
+export NN_HOME="$COSYNN_HOME/nn"
+export LOG_DIR="$NN_HOME/logs"
+export PYTHONPATH="$COSYNN_HOME:$PYTHONPATH"
+export PYTHONPATH="$NN_HOME:$PYTHONPATH"
+export DATAPATH="$COSYNN_HOME/data"
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64
 #source activate hgcn  # replace with source hgcn/bin/activate if you used a virtualenv
